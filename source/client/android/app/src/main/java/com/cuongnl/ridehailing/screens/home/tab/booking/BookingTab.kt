870fa93272770acf98bae9246f12e17711d7df10
cuong_nl <cuonglc@vtdglobal.com>
2023-11-01 15:13:51 +0700

    where do you want to go

--- source/client/android/app/src/main/java/com/cuongnl/ridehailing/screens/home/tab/booking/BookingTab.kt
+++ source/client/android/app/src/main/java/com/cuongnl/ridehailing/screens/home/tab/booking/BookingTab.kt
@@ -2,9 +2,12 @@ package com.cuongnl.ridehailing.screens.home.tab.booking
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.cuongnl.ridehailing.screens.home.tab.booking.ui.DashboardBanner
+import com.cuongnl.ridehailing.screens.home.tab.booking.ui.WhereDoYouWantToGo
+import ir.kaaveh.sdpcompose.sdp
 
 @Composable
 fun BookingTab() {
@@ -13,5 +16,13 @@ fun BookingTab() {
             .fillMaxSize()
     ) {
         DashboardBanner()
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 10.sdp)
+        ) {
+            WhereDoYouWantToGo()
+        }
     }
 }
