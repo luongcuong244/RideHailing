4fa191c8581d00d26ac910678614654f308548eb
cuong_nl <luongcuong244@gmail.com>
2023-11-03 23:15:13 +0700

    update mongodb host

--- source/server/config/database.js
+++ source/server/config/database.js
@@ -4,7 +4,7 @@ require("dotenv").config()
 const connectDatabase = async ()=>{
 
     try {
-        const databaseConfig = process.env.MONGODB_URI;       
+        const databaseConfig = "mongodb+srv://luongcuong2002:Matkhau123@cluster0.1vrhkci.mongodb.net/";       
         const connect = await mongoose.connect(databaseConfig);
         console.log(`da ket noi mongodb: ${connect.connection.host}`);
     } catch (error) {
